eaf4853c0dbca83a573703bc9f683687f75c4f52
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-11-15 11:01:49 +1300

    no longer necessary to fix scitypes in iris data set

--- notebooks/02_models/notebook.pluto.jl
+++ notebooks/02_models/notebook.pluto.jl
@@ -1,8 +1,49 @@
 ### A Pluto.jl notebook ###
-# v0.16.0
+# v0.17.1
 
 using Markdown
 using InteractiveUtils
+
+# ╔═╡ d09256dd-6c0d-4e28-9e54-3f7b3ca87ecb
+begin
+  using Pkg
+  Pkg.activate("env")
+  Pkg.instantiate()
+end
+
+# ╔═╡ ae4f097b-4c18-4025-8514-99938a2932db
+begin
+  using MLJ
+  OpenML.describe_dataset(61)
+end
+
+# ╔═╡ 88c11aa3-2c4c-4200-a0de-1721c1bc2df2
+begin
+  iris = OpenML.load(61); # a column dictionary table
+  
+  using DataFrames
+  iris = DataFrames.DataFrame(iris);
+  first(iris, 4)
+end
+
+# ╔═╡ 6df9f266-f9d9-4506-b8ad-0340f15a03ba
+begin
+  using UrlDownload, CSV
+  csv_file = urldownload("https://raw.githubusercontent.com/ablaom/"*
+                     "MachineLearningInJulia2020/"*
+                     "for-MLJ-version-0.16/data/horse.csv");
+  horse = DataFrames.DataFrame(csv_file); # convert to data frame
+  coerce!(horse, autotype(horse));
+  coerce!(horse, Count => Continuous);
+  coerce!(horse,
+          :surgery               => Multiclass,
+          :age                   => Multiclass,
+          :mucous_membranes      => Multiclass,
+          :capillary_refill_time => Multiclass,
+          :outcome               => Multiclass,
+          :cp_data               => Multiclass);
+  schema(horse)
+end
 
 # ╔═╡ a05a4f6d-c831-4fb1-9bca-7c69b794f8ce
 md"# Machine Learning in Julia, JuliaCon2020"
@@ -30,13 +71,6 @@ md"""
 The package environment has been created using **Julia 1.6** and may not
 instantiate properly for other Julia versions.
 """
-
-# ╔═╡ d09256dd-6c0d-4e28-9e54-3f7b3ca87ecb
-begin
-  using Pkg
-  Pkg.activate("env")
-  Pkg.instantiate()
-end
 
 # ╔═╡ 499cbc31-83ba-4583-ba1f-6363f43ec697
 md"## General resources"
@@ -70,21 +104,6 @@ famous iris data set. This time, we'll grab the data from
 [OpenML](https://www.openml.org):
 """
 
-# ╔═╡ ae4f097b-4c18-4025-8514-99938a2932db
-begin
-  using MLJ
-  OpenML.describe_dataset(61)
-end
-
-# ╔═╡ 88c11aa3-2c4c-4200-a0de-1721c1bc2df2
-begin
-  iris = OpenML.load(61); # a column dictionary table
-  
-  using DataFrames
-  iris = DataFrames.DataFrame(iris);
-  first(iris, 4)
-end
-
 # ╔═╡ 27b51025-5fc0-4453-bca8-7eec7950fd82
 md"""
 **Main goal.** To build and evaluate models for predicting the
@@ -115,12 +134,6 @@ is needed for MLJ supervised models. We randomize the data at the
 same time:
 """
 
-# ╔═╡ 933cc42f-6be5-4b9b-a367-9aa3c6cf34d0
-begin
-  y, X = unpack(iris, ==(:class), name->true; rng=123);
-  scitype(y)
-end
-
 # ╔═╡ b081e89d-3ded-4061-bf32-94657e65284e
 md"""
 Here's one way to access the documentation (at the REPL, `?unpack`
@@ -148,9 +161,6 @@ meta = all_models[3]
 # ╔═╡ 2640cb64-bc0e-4bca-81bc-2d5603785a09
 targetscitype = meta.target_scitype
 
-# ╔═╡ ba53ad4c-e092-40fd-9d87-1d1fbb0e3997
-scitype(y) <: targetscitype
-
 # ╔═╡ 5e706d1f-93f3-4d50-b950-fddef2a1fb10
 md"So this model won't do. Let's  find all pure julia classifiers:"
 
@@ -171,9 +181,6 @@ models("Classifier")
 
 # ╔═╡ a42a2993-098f-4943-a87d-950c50fb2955
 md"Find all (supervised) models that match my data!"
-
-# ╔═╡ db2d9ba2-1fc4-4a7f-8448-4bcb089096cd
-models(matching(X, y))
 
 # ╔═╡ 34feba2c-e700-423a-a012-a84240254fb6
 md"### Step 3. Select and instantiate a model"
@@ -230,9 +237,6 @@ In MLJ a model and training/validation data are typically bound
 together in a machine:
 """
 
-# ╔═╡ 72510848-3148-408a-895b-997a92b731e0
-mach = machine(model, X, y)
-
 # ╔═╡ c8700a21-e95b-4f0f-a525-e9b04a4bd246
 md"""
 A machine stores *learned* parameters, among other things. We'll
@@ -241,23 +245,11 @@ set. Let's start by dividing all row indices into `train` and `test`
 subsets:
 """
 
-# ╔═╡ 5b8d30eb-bd5c-4b09-80f0-ba7781e173cf
-train, test = partition(eachindex(y), 0.7)
-
 # ╔═╡ a232a792-8692-45cc-9cbc-9a2c39793333
 md"Now we can `fit!`..."
 
-# ╔═╡ 62be4fb6-3b7e-4b80-b887-2b16710e5502
-fit!(mach, rows=train, verbosity=2)
-
 # ╔═╡ 3856cc2d-ea15-4077-9452-5298a8a4a401
 md"... and `predict`:"
-
-# ╔═╡ 5a86fd44-8e9b-4694-b017-96f1602f2cad
-begin
-  yhat = predict(mach, rows=test);  # or `predict(mach, Xnew)`
-  yhat[1:3]
-end
 
 # ╔═╡ 7d8e68c8-430c-46dd-8be2-2a4017c45345
 md"We'll have more to say on the form of this prediction shortly."
@@ -265,42 +257,20 @@ md"We'll have more to say on the form of this prediction shortly."
 # ╔═╡ fb9a3a2d-6b1a-4d64-a7ad-80d84f5b0070
 md"After training, one can inspect the learned parameters:"
 
-# ╔═╡ deef14fe-9822-4fa8-8378-5aa686f0b407
-fitted_params(mach)
-
 # ╔═╡ d5308047-a4ee-41c8-9f43-49763e8691a1
 md"""
 Everything else the user might be interested in is accessed from the
 training *report*:
 """
 
-# ╔═╡ d8f73aeb-2b82-4afd-bb0e-3a45761c733a
-report(mach)
-
 # ╔═╡ 2ecd9ccb-45c4-4de4-9979-7044b2f2df33
 md"You save a machine like this:"
-
-# ╔═╡ 43887c78-378f-4bb8-b2a1-fbbde543f620
-MLJ.save("neural_net.jlso", mach)
 
 # ╔═╡ 43c25729-4bda-4c21-910b-f8a9a217eff2
 md"And retrieve it like this:"
 
-# ╔═╡ de239414-32a8-4691-aa36-d2e8546da46a
-begin
-  mach2 = machine("neural_net.jlso")
-  yhat = predict(mach2, X);
-  yhat[1:3]
-end
-
 # ╔═╡ 2a1d3e22-d17c-452d-88a1-a2bf91434413
 md"If you want to fit a retrieved model, you will need to bind some data to it:"
-
-# ╔═╡ ef2282bc-db28-4d54-a1cb-54e34396a855
-begin
-  mach3 = machine("neural_net.jlso", X, y)
-  fit!(mach3)
-end
 
 # ╔═╡ 9dbeeed8-d882-4e04-8037-0de5806e1a54
 md"""
@@ -310,36 +280,17 @@ computations in the case that only the iteration parameter is
 increased:
 """
 
-# ╔═╡ 7547798a-24c9-4c5f-9961-a09632c33fb0
-begin
-  model.epochs = model.epochs + 4
-  fit!(mach, rows=train, verbosity=2)
-end
-
 # ╔═╡ 8ce96a51-938d-43d1-b7cc-46f4ef988c68
 md"""
 For this particular model we can also increase `:learning_rate`
 without triggering a cold restart:
 """
 
-# ╔═╡ 04255053-ed5f-48d6-90fb-7f2721f6fcc7
-begin
-  model.epochs = model.epochs + 4
-  model.optimiser.eta = 10*model.optimiser.eta
-  fit!(mach, rows=train, verbosity=2)
-end
-
 # ╔═╡ 35af5a52-69f9-4b74-af65-ccd25ecb9818
 md"""
 However, change any other parameter and training will restart from
 scratch:
 """
-
-# ╔═╡ 55531d1d-31e9-4e61-8890-979691212d9b
-begin
-  model.lambda = 0.001
-  fit!(mach, rows=train, verbosity=2)
-end
 
 # ╔═╡ 87065854-efb2-44b7-a6fa-eaf14df5d44b
 md"""
@@ -355,14 +306,6 @@ md"""
 Let's train silently for a total of 50 epochs, and look at a
 prediction:
 """
-
-# ╔═╡ df4bd436-9b0e-484b-9e90-09023d201062
-begin
-  model.epochs = 50
-  fit!(mach, rows=train)
-  yhat = predict(mach, X[test,:]); # or predict(mach, rows=test)
-  yhat[1]
-end
 
 # ╔═╡ 2faf541c-1a2c-4274-b7bb-2f33ef765cc0
 md"What's going on here?"
@@ -383,23 +326,11 @@ So, to obtain the probability of "Iris-virginica" in the first test
 prediction, we do
 """
 
-# ╔═╡ 1613d417-80ac-40bd-a6e5-2f0b4dca5c59
-pdf(yhat[1], "Iris-virginica")
-
 # ╔═╡ b760d242-43de-4248-8550-20498aa03ed0
 md"To get the most likely observation, we do"
 
-# ╔═╡ f40a1541-ee37-487e-9e7b-5b943cf6b560
-mode(yhat[1])
-
 # ╔═╡ 523bcb12-f95d-4b90-bce6-0a1379cc1259
 md"These can be broadcast over multiple predictions in the usual way:"
-
-# ╔═╡ 691b9fb3-72c7-43bd-9608-b5032c116833
-broadcast(pdf, yhat[1:4], "Iris-versicolor")
-
-# ╔═╡ 880f9d8d-3095-45b5-b473-4aa968e6937a
-mode.(yhat[1:4])
 
 # ╔═╡ 275f2017-5520-4072-8d9e-644a1b3cc6b6
 md"""
@@ -407,17 +338,8 @@ Or, alternatively, you can use the `predict_mode` operation instead
 of `predict`:
 """
 
-# ╔═╡ 93533a8e-e507-4950-ac0a-23ded81445da
-predict_mode(mach, X[test,:])[1:4] # or predict_mode(mach, rows=test)[1:4]
-
 # ╔═╡ 1d455c06-389b-402a-8535-1a088a6a3228
 md"For a more conventional matrix of probabilities you can do this:"
-
-# ╔═╡ 76bc1d1e-3de7-461c-a39f-7893c73eef39
-begin
-  L = levels(y)
-  pdf(yhat, L)[1:4, :]
-end
 
 # ╔═╡ bf534937-b3c5-4e01-bcca-51a27994a151
 md"""
@@ -426,14 +348,8 @@ might imagine. In particular, all probabilistic performance measures
 in MLJ expect distribution objects in their first slot:
 """
 
-# ╔═╡ 8d59229c-1a70-40b2-9b33-09b4b6661170
-cross_entropy(yhat, y[test]) |> mean
-
 # ╔═╡ 8b98a48c-d2c6-4ecf-b45d-88d068bb0fa2
 md"To apply a deterministic measure, we first need to obtain point-estimates:"
-
-# ╔═╡ 0fb6fa7a-f0c0-4926-92c8-6db3a590d963
-misclassification_rate(mode.(yhat), y[test])
 
 # ╔═╡ 09a60a33-8b8d-4aed-abf5-96f457eb2bdf
 md"""
@@ -454,27 +370,14 @@ evaluation with a lot less fuss. Let's repeat the performance
 evaluation above and add an extra measure, `brier_score`:
 """
 
-# ╔═╡ c63ed6db-6d2a-4ee7-9b21-6d4eb642d87e
-evaluate!(mach, resampling=Holdout(fraction_train=0.7),
-          measures=[cross_entropy, brier_score])
-
 # ╔═╡ f829c00b-f082-4ec4-b98c-3df1f31879bf
 md"Or applying cross-validation instead:"
-
-# ╔═╡ 727ecfae-2e87-4e05-92b7-4ceba56e97ad
-evaluate!(mach, resampling=CV(nfolds=6),
-          measures=[cross_entropy, brier_score])
 
 # ╔═╡ 31359fe0-8d95-4740-b122-214de244406c
 md"""
 Or, Monte Carlo cross-validation (cross-validation repeated
 randomized folds)
 """
-
-# ╔═╡ 1383b285-59f2-4652-8a4b-c83694978e38
-e = evaluate!(mach, resampling=CV(nfolds=6, rng=123),
-              repeats=3,
-              measures=[cross_entropy, brier_score])
 
 # ╔═╡ 3c3b7c0f-ef92-4eeb-a8b6-f7ff516dedc4
 md"""
@@ -492,17 +395,6 @@ example, imagining the last 30% of target observations are `missing`
 you might have a work-flow like this:
 """
 
-# ╔═╡ 174b9a92-2cf5-4630-a04e-0b76409c14a7
-begin
-  train, test = partition(eachindex(y), 0.7)
-  mach = machine(model, X, y)
-  evaluate!(mach, resampling=CV(nfolds=6),
-            measures=[cross_entropy, brier_score],
-            rows=train)     # cv estimate, resampling from `train`
-  fit!(mach, rows=train)    # re-train using all of `train` observations
-  predict(mach, rows=test); # and predict missing targets
-end
-
 # ╔═╡ 51f56327-7309-4aa2-b979-3458f2f26667
 md"### On learning curves"
 
@@ -518,22 +410,6 @@ starts by defining a one-dimensional range object for the parameter
 
 # ╔═╡ 1d65816d-d818-434d-b10e-562ce21caa04
 r = range(model, :epochs, lower=1, upper=50, scale=:log)
-
-# ╔═╡ 97400000-e067-4c66-8f78-ab549ef1544e
-begin
-  curve = learning_curve(mach,
-                         range=r,
-                         resampling=Holdout(fraction_train=0.7), # (default)
-                         measure=cross_entropy)
-  
-  using Plots
-  gr(size=(490,300))
-  plt=plot(curve.parameter_values, curve.measurements)
-  xlabel!(plt, "epochs")
-  ylabel!(plt, "cross entropy on holdout set")
-  savefig("learning_curve.png")
-  plt
-end
 
 # ╔═╡ e80b6400-2c9e-4198-a8a4-a53f5149481e
 md"We will return to learning curves when we look at tuning in Part 4."
@@ -607,13 +483,103 @@ begin
   pretty(data)
 end
 
-# ╔═╡ a1c60d89-5d61-4a26-b61c-748aec38e674
+# ╔═╡ ba53ad4c-e092-40fd-9d87-1d1fbb0e3997
+scitype(y) <: targetscitype
+
+# ╔═╡ db2d9ba2-1fc4-4a7f-8448-4bcb089096cd
+models(matching(X, y))
+
+# ╔═╡ ef2282bc-db28-4d54-a1cb-54e34396a855
 begin
-  using Tables
-  y, X, w = unpack(data,
-                   ==(:a),
-                   name -> elscitype(Tables.getcolumn(data, name)) == Continuous,
-                   name -> true);
+  mach3 = machine("neural_net.jlso", X, y)
+  fit!(mach3)
+end
+
+# ╔═╡ 62be4fb6-3b7e-4b80-b887-2b16710e5502
+fit!(mach, rows=train, verbosity=2)
+
+# ╔═╡ deef14fe-9822-4fa8-8378-5aa686f0b407
+fitted_params(mach)
+
+# ╔═╡ d8f73aeb-2b82-4afd-bb0e-3a45761c733a
+report(mach)
+
+# ╔═╡ 43887c78-378f-4bb8-b2a1-fbbde543f620
+MLJ.save("neural_net.jlso", mach)
+
+# ╔═╡ 7547798a-24c9-4c5f-9961-a09632c33fb0
+begin
+  model.epochs = model.epochs + 4
+  fit!(mach, rows=train, verbosity=2)
+end
+
+# ╔═╡ 04255053-ed5f-48d6-90fb-7f2721f6fcc7
+begin
+  model.epochs = model.epochs + 4
+  model.optimiser.eta = 10*model.optimiser.eta
+  fit!(mach, rows=train, verbosity=2)
+end
+
+# ╔═╡ 55531d1d-31e9-4e61-8890-979691212d9b
+begin
+  model.lambda = 0.001
+  fit!(mach, rows=train, verbosity=2)
+end
+
+# ╔═╡ 1613d417-80ac-40bd-a6e5-2f0b4dca5c59
+pdf(yhat[1], "Iris-virginica")
+
+# ╔═╡ f40a1541-ee37-487e-9e7b-5b943cf6b560
+mode(yhat[1])
+
+# ╔═╡ 691b9fb3-72c7-43bd-9608-b5032c116833
+broadcast(pdf, yhat[1:4], "Iris-versicolor")
+
+# ╔═╡ 880f9d8d-3095-45b5-b473-4aa968e6937a
+mode.(yhat[1:4])
+
+# ╔═╡ 76bc1d1e-3de7-461c-a39f-7893c73eef39
+begin
+  L = levels(y)
+  pdf(yhat, L)[1:4, :]
+end
+
+# ╔═╡ 93533a8e-e507-4950-ac0a-23ded81445da
+predict_mode(mach, X[test,:])[1:4] # or predict_mode(mach, rows=test)[1:4]
+
+# ╔═╡ 8d59229c-1a70-40b2-9b33-09b4b6661170
+cross_entropy(yhat, y[test]) |> mean
+
+# ╔═╡ 0fb6fa7a-f0c0-4926-92c8-6db3a590d963
+misclassification_rate(mode.(yhat), y[test])
+
+# ╔═╡ c63ed6db-6d2a-4ee7-9b21-6d4eb642d87e
+evaluate!(mach, resampling=Holdout(fraction_train=0.7),
+          measures=[cross_entropy, brier_score])
+
+# ╔═╡ 727ecfae-2e87-4e05-92b7-4ceba56e97ad
+evaluate!(mach, resampling=CV(nfolds=6),
+          measures=[cross_entropy, brier_score])
+
+# ╔═╡ 1383b285-59f2-4652-8a4b-c83694978e38
+e = evaluate!(mach, resampling=CV(nfolds=6, rng=123),
+              repeats=3,
+              measures=[cross_entropy, brier_score])
+
+# ╔═╡ 97400000-e067-4c66-8f78-ab549ef1544e
+begin
+  curve = learning_curve(mach,
+                         range=r,
+                         resampling=Holdout(fraction_train=0.7), # (default)
+                         measure=cross_entropy)
+  
+  using Plots
+  gr(size=(490,300))
+  plt=plot(curve.parameter_values, curve.measurements)
+  xlabel!(plt, "epochs")
+  ylabel!(plt, "cross entropy on holdout set")
+  savefig("learning_curve.png")
+  plt
 end
 
 # ╔═╡ b5b23e79-35ce-4857-9727-ed822e4fd85d
@@ -636,25 +602,6 @@ md"""
 Here is the Horse Colic data introduced in Part 1, together with the
 type coercions we performed there:
 """
-
-# ╔═╡ 6df9f266-f9d9-4506-b8ad-0340f15a03ba
-begin
-  using UrlDownload, CSV
-  csv_file = urldownload("https://raw.githubusercontent.com/ablaom/"*
-                     "MachineLearningInJulia2020/"*
-                     "for-MLJ-version-0.16/data/horse.csv");
-  horse = DataFrames.DataFrame(csv_file); # convert to data frame
-  coerce!(horse, autotype(horse));
-  coerce!(horse, Count => Continuous);
-  coerce!(horse,
-          :surgery               => Multiclass,
-          :age                   => Multiclass,
-          :mucous_membranes      => Multiclass,
-          :capillary_refill_time => Multiclass,
-          :outcome               => Multiclass,
-          :cp_data               => Multiclass);
-  schema(horse)
-end
 
 # ╔═╡ 0afa73e6-f371-4b04-946d-b124a8db5f7c
 md"""
@@ -742,6 +689,59 @@ md"""
 
 *This notebook was generated using [Literate.jl](https://github.com/fredrikekre/Literate.jl).*
 """
+
+# ╔═╡ de239414-32a8-4691-aa36-d2e8546da46a
+begin
+  mach2 = machine("neural_net.jlso")
+  yhat = predict(mach2, X);
+  yhat[1:3]
+end
+
+# ╔═╡ df4bd436-9b0e-484b-9e90-09023d201062
+begin
+  model.epochs = 50
+  fit!(mach, rows=train)
+  yhat = predict(mach, X[test,:]); # or predict(mach, rows=test)
+  yhat[1]
+end
+
+# ╔═╡ 174b9a92-2cf5-4630-a04e-0b76409c14a7
+begin
+  train, test = partition(eachindex(y), 0.7)
+  mach = machine(model, X, y)
+  evaluate!(mach, resampling=CV(nfolds=6),
+            measures=[cross_entropy, brier_score],
+            rows=train)     # cv estimate, resampling from `train`
+  fit!(mach, rows=train)    # re-train using all of `train` observations
+  predict(mach, rows=test); # and predict missing targets
+end
+
+# ╔═╡ 933cc42f-6be5-4b9b-a367-9aa3c6cf34d0
+begin
+  y, X = unpack(iris, ==(:class), name->true; rng=123);
+  scitype(y)
+end
+
+# ╔═╡ a1c60d89-5d61-4a26-b61c-748aec38e674
+begin
+  using Tables
+  y, X, w = unpack(data,
+                   ==(:a),
+                   name -> elscitype(Tables.getcolumn(data, name)) == Continuous,
+                   name -> true);
+end
+
+# ╔═╡ 5b8d30eb-bd5c-4b09-80f0-ba7781e173cf
+train, test = partition(eachindex(y), 0.7)
+
+# ╔═╡ 72510848-3148-408a-895b-997a92b731e0
+mach = machine(model, X, y)
+
+# ╔═╡ 5a86fd44-8e9b-4694-b017-96f1602f2cad
+begin
+  yhat = predict(mach, rows=test);  # or `predict(mach, Xnew)`
+  yhat[1:3]
+end
 
 # ╔═╡ Cell order:
 # ╟─a05a4f6d-c831-4fb1-9bca-7c69b794f8ce
